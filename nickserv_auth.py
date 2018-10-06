3400288316639781752138f8e9812166b8a93bc7
Frank LENORMAND <lenormf@gmail.com>
2018-10-06 11:12:24 +0300

    nickserv_auth: Capture nicknames dynamically in registration notices

--- nickserv_auth.py
+++ nickserv_auth.py
@@ -7,8 +7,8 @@
 import irc3
 
 
-@irc3.event(r'(@(?P<tags>\S+) )?:(?P<ns>NickServ)!NickServ@services.'
-            r' NOTICE (?P<nick>irc3) :This nickname is registered.*')
+@irc3.event(r'(@(?P<tags>\S+) )?:(?P<ns>.+)!NickServ@services.'
+            r' NOTICE (?P<nick>.+) :This nickname is registered.*')
 def register(bot, ns=None, nick=None, **kw):
     if bot.config["password"]:
         bot.privmsg(ns, 'identify %s %s' % (nick, bot.config["password"]))
